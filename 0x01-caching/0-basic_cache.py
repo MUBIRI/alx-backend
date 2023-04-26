f4cead042012c2dfadba8dd2043ab6bbb0f06745
MUBIRI <mubirirobberts@gmail.com>
2023-04-26 13:38:17 +0000

    Cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,14 +8,20 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-'''BasicCache'''
+    '''A class `BasicCache` that inherits from `BaseCaching`
+       and is a caching system
+    '''
 
     def put(self, key, item):
-        '''assigning a dict to a function'''
+        '''assign to the dictionary `self.cache_data` the
+           `item` value for the key `key`
+        '''
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        '''getting the function'''
+        '''return the value in `self.cache_data` linked to `key`
+        '''
 
         return self.cache_data.get(key, None)
+
